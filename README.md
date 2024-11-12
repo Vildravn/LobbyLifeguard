8f515dca273c8d456282af5da9b5425596edae70
Vildravn <vildravn@gmail.com>
2024-11-12 19:59:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# LobbyLifeguard
+
+A lifeguard for your lobby that bans icky people on sight.
+
+This mod is intended to stop unwelcome people that grief WEBFISHING lobbies by guessing lobby codes. It will automatically ban anyone configured in the banlist upon joining the lobby.
+
+## Installation
+
+Download the [latest release](https://github.com/Vildravn/LobbyLifeguard/releases/latest/download/LobbyLifeguard.zip) and extract it to your game install.
+
+You can also [install from Thunderstore](https://thunderstore.io/c/webfishing/p/Vildravn/LobbyLifeguard/).
+
+## Usage
+
+Open LobbyLifeguard's settings by clicking on the Configure button in its entry in the Mods list.
+
+The banlist field accepts a comma separated list of Steam IDs.
+
+The banlist will update immediately upon saving changes, no game restart needed.
+
+## Planned features
+
+The following features are not yet implemented but I am planning to add them:
+
+* Automatically add a person to the banlist when banned through the player list in-game
+
+## Credits
+
+LobbyLifeguard's logo is U+1F6Df "Ring Buoy" in [Twemoji](https://github.com/jdecked/twemoji).
---
+++ project/mods/LobbyLifeguard/main.gd
@@ -0,0 +1,46 @@
+extends Node
+
+const MOD_ID = "LobbyLifeguard"
+
+const DEFAULT_CONFIG = {"banlist": ""}
+var config: Dictionary
+var banlist: Array
+
+onready var TackleBox := $"/root/TackleBox"
+
+
+func _ready():
+	TackleBox.connect("mod_config_updated", self, "_on_config_update")
+	Network.connect("_user_connected", self, "_on_player_connect")
+	_init_config()
+
+
+func _init_config():
+	config = TackleBox.get_mod_config(MOD_ID)
+	if not config["banlist"]:
+		config = DEFAULT_CONFIG
+		TackleBox.set_mod_config(MOD_ID, config)
+	
+	_populate_banlist()
+
+
+func _on_config_update(mod_id, new_config):
+	if mod_id != MOD_ID: return
+	
+	_populate_banlist()
+
+
+func _on_player_connect(steam_id):
+	var str_steam_id = String(steam_id)
+	if str_steam_id in banlist:
+		print("[%s] Player %s found on the banlist. Banning them." % [MOD_ID, steam_id])
+		var banned_username = Network._get_username_from_id(steam_id)
+		Network._ban_player(steam_id)
+		Network._update_chat("[color=#ac0029][%s]Player banned, they will be removed from the online list soon.[/color]" % [MOD_ID, banned_username])
+
+
+func _populate_banlist():
+	if config["banlist"]:
+		banlist = []
+		for entry in config["banlist"].split(","):
+			banlist.append(entry.strip_edges())
